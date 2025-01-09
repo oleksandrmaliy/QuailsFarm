e388d674319bd947756f384d170a89972422c3da
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-01-10 00:51:38 +0200

    fixed gallery

--- src/components/photos.jsx
+++ src/components/photos.jsx
@@ -8,51 +8,27 @@ export const Photos = () => {
         <div className="text-2xl">
           <ul className="grid grid-cols-2 gap-8">
             <li className="border-2 border-green-900 shadow-md shadow-green-500 rounded-xl overflow-hidden">
-              <img
-                src="./public/quail1.jpg"
-                alt="Фото"
-                className="w-full h-auto"
-              />
+              <img src="./quail1.jpg" alt="Фото" className="w-full h-auto" />
               <p className="text-center">Перепелка</p>
             </li>
             <li className="border-2 border-green-900 shadow-md shadow-green-500 rounded-xl overflow-hidden">
-              <img
-                src="./public/quail2.jpg"
-                alt="Фото"
-                className="w-full h-auto"
-              />
+              <img src="./quail2.jpg" alt="Фото" className="w-full h-auto" />
               <p className="text-center">Яєчки, да</p>
             </li>
             <li className="border-2 border-green-900 shadow-md shadow-green-500 rounded-xl overflow-hidden">
-              <img
-                src="./public/quail3.jpg"
-                alt="Фото"
-                className="w-full h-auto"
-              />
+              <img src="./quail3.jpg" alt="Фото" className="w-full h-auto" />
               <p className="text-center">Перепелки смачні</p>
             </li>
             <li className="border-2 border-green-900 shadow-md shadow-green-500 rounded-xl overflow-hidden">
-              <img
-                src="./public/quail4.jpg"
-                alt="Фото"
-                className="w-full h-auto"
-              />
+              <img src="./quail4.jpg" alt="Фото" className="w-full h-auto" />
               <p className="text-center">Перепел з яйцями</p>
             </li>
             <li className="border-2 border-green-900 shadow-md shadow-green-500 rounded-xl overflow-hidden">
-              <img
-                src="./public/quail5.jpg"
-                alt="Фото"
-                className="w-full h-auto"
-              />
+              <img src="./quail5.jpg" alt="Фото" className="w-full h-auto" />
               <p className="text-center">Перепелка смачна</p>
             </li>
             <li className="border-2 border-green-900 shadow-md shadow-green-500 rounded-xl overflow-hidden">
-              <img
-                src="./public/quail6.jpg"
-                alt="Фото"
-                className="w-full h-auto"
-              />
+              <img src="./quail6.jpg" alt="Фото" className="w-full h-auto" />
               <p className="text-center">Яєчки: а шо це, га?</p>
             </li>
           </ul>
