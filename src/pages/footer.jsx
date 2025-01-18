d14d2e63b060ced7365e6345074b7df9a9037e3d
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-01-18 19:20:45 +0200

    fix footer

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,31 +1,34 @@
+import { Container } from '../components/container';
+
 export const Footer = () => {
   return (
-    <footer className="flex items-center p-4">
-      <div className="min-w-[1200px] flex flex-row items-center gap-6 p-6 mx-auto border-green-700 border-2 rounded-3xl">
-        <a
-          href="https://www.instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="./igrd.svg" alt="SVG Image" width="48" height="48" />
-        </a>
-        <a
-          href="https://www.tiktok.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="./ttrd.svg" alt="SVG Image" width="48" height="48" />
-        </a>
-        <a
-          href="https://www.facebook.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="./facebook.svg" alt="SVG Image" width="48" height="48" />
-        </a>
-
-        <h3 className="text-3xl ml-auto">Powered by MaliyCo</h3>
-      </div>
+    <footer className="flex items-center p-2 mb-4 bg-green-500">
+      <Container>
+        <div className="w-full max-w-[639px] mx-auto flex flex-row items-center gap-2 p-2 rounded-xl bg-yellow-100">
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./igrd.svg" alt="SVG Image" width="48" height="48" />
+          </a>
+          <a
+            href="https://www.tiktok.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./ttrd.svg" alt="SVG Image" width="48" height="48" />
+          </a>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./facebook.svg" alt="SVG Image" width="48" height="48" />
+          </a>
+          <h3 className="text-3xl ml-auto">Powered by MaliyCo</h3>
+        </div>
+      </Container>
     </footer>
   );
 };
