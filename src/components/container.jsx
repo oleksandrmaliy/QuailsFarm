46e342181c63d0a5a907ed9c4127373ddaaf35e3
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-01-23 12:55:38 +0200

    eggs and meat

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,7 +1,3 @@
 export const Container = ({ children }) => {
-  return (
-    <div className="w-full max-w-[639px] px-4 mx-auto font-roboto">
-      {children}
-    </div>
-  );
+  return <div className="w-full max-w-[639px] px-4 mx-auto">{children}</div>;
 };
