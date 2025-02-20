dda86219355406fca45d7889e69308200f42b8e4
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-02-20 17:36:55 +0200

    thursday two

--- src/components/advertising.jsx
+++ src/components/advertising.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import adimgone from '../assets/images/adimgone.jpg';
+import adimgtwo from '../assets/images/adimgtwo.jpg';
 
-import AdsOne from './ads';
-import AdsTwo from './ads';
+import AdOne from './ads';
+import AdTwo from './ads';
 
 export const Advertising = () => {
   const [adOne, setAdOne] = useState(true);
@@ -9,79 +11,28 @@ export const Advertising = () => {
 
   const textOne = 'Це реклама номер один';
   const urlOne = '#';
-  const imgOne = '../../src/assets/alimgone.jpg';
 
   const textTwo = 'Це реклама номер дра';
   const urlTwo = '#';
-  const imgTwo = '../../src/assets/alimgtwo.jpg';
 
   return (
     <section className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      {/* <div className="w-full p-4 bg-primaryColor rounded-xl overflow-hidden flex flex-col justify-between">
-        <div className="text-bgColor mb-4">
-          <p>Реклама від партнерів: </p>
-          <b>
-            Тут має бути ваша реклама!
-            <br />
-            Тут обов`язково має бути ваша реклама!
-          </b>
-        </div>
-        <div className="w-full shadow-allYellow rounded-xl overflow-hidden hover:scale-[1.01] transition-transform duration-300 ease-in-out">
-          <a
-            href="#"
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-            className="block cursor-pointer rounded-2xl"
-          >
-            <img
-              src="/blank.jpg"
-              alt="Big View"
-              className="w-full h-auto object-cover mt-auto"
-            />
-          </a>
-        </div>
-      </div> */}
       {adOne && (
-        <AdsOne
-          img={imgOne}
+        <AdOne
+          img={adimgone}
           url={urlOne}
           text={textOne}
           setAdState={setAdOne}
         />
       )}
       {adTwo && (
-        <AdsTwo
-          img={imgTwo}
+        <AdTwo
+          img={adimgtwo}
           url={urlTwo}
           text={textTwo}
           setAdState={setAdTwo}
         />
       )}
-
-      {/* <div className="w-full p-4 bg-primaryColor rounded-xl overflow-hidden flex flex-col justify-between">
-        <div className="text-bgColor mb-4">
-          <p>Реклама від партнерів: </p>
-          <b>
-            Тут має бути ваша реклама!
-            <br />
-            Тут обов`язково має бути ваша реклама!
-          </b>
-        </div>
-        <div className="w-full shadow-allYellow rounded-xl overflow-hidden hover:scale-[1.01] transition-transform duration-300 ease-in-out">
-          <a
-            href="#"
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-            className="block cursor-pointer rounded-2xl"
-          >
-            <img
-              src="/blank.jpg"
-              alt="Big View"
-              className="w-full h-auto object-cover mt-auto"
-            />
-          </a>
-        </div>
-      </div> */}
     </section>
   );
 };
